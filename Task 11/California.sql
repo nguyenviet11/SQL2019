a719f05068a976777ee404d34071425ea0f6339d
Viet <nguyenvanvietbn1998@gmail.com>
2021-07-27 22:08:13 +0700

    ss

---
+++ Task 11/California.sql
@@ -0,0 +1,136 @@
+﻿CREATE DATABASE California
+
+USE California
+GO
+CREATE TABLE Toys(
+ProductCode varchar(5) PRIMARY KEY,
+Name varchar(30),
+Category varchar(30),
+Manufacturer varchar(30),
+AgeRange varchar(15),
+UnitPrice money,
+Netweight int,
+QtyOnHand int
+)
+INSERT INTO Toys VALUES ('A100A','Ô tô','Lắp ráp','Toyota','5+',4000,400,70)
+INSERT INTO Toys VALUES ('A101A','Xe máy','Lắp ráp','Honda','5+',6000,500,40)
+INSERT INTO Toys VALUES ('A102A','Xe đạp','Lắp ráp','Honda','5+',6000,500,20)
+INSERT INTO Toys VALUES ('A103A','Xích lô','Lắp ráp','Toyota','5+',4000,900,40)
+INSERT INTO Toys VALUES ('A104A','Container','Bộ Game','Mitsubishi','5+',6000,450,30)
+GO
+INSERT INTO Toys VALUES ('A105A','Dứa','Bộ Game','Asus','15+',4000,400,30)
+INSERT INTO Toys VALUES ('A106A','Lê ','Bộ Game','Asus','15+',1500,400,70)
+INSERT INTO Toys VALUES ('A107A','Táo','Bộ Game','Asus','5+',8500,400,80)
+INSERT INTO Toys VALUES ('A108A','Xoài','Lắp ráp','Samsung','5+',6000,400,50)
+INSERT INTO Toys VALUES ('A109A','Lợn','Lắp ráp','Toyota','8+',400,470,90)
+GO
+INSERT INTO Toys VALUES ('A110A','Khủng Long','Câu đổ','Toyota','7+',7800,400,40)
+INSERT INTO Toys VALUES ('A111A','Rubik','Lắp ráp','Toyota','5+',4960,400,40)
+INSERT INTO Toys VALUES ('A112A','Xếp hình','Lắp ráp','Toyota','9+',3000,400,40)
+INSERT INTO Toys VALUES ('A113A','Yo yo','Câu đố','Toyota','5+',2000,490,30)
+INSERT INTO Toys VALUES ('A114A','cần cẩu','Câu đố','Toyota','5+',2000,700,40)
+INSERT INTO Toys VALUES ('A115A','cần cẩu','Câu đố','Toyota','5+',2000,700,40)
+
+--2
+CREATE PROCEDURE Heavytoys 
+	@name int
+AS
+SELECT * FROM Toys
+WHERE Netweight > @name
+GO
+EXEC Heavytoys 100
+
+
+drop proc Heavytoys
+--3
+CREATE PROCEDURE PriceIncrease
+	@Unitprice money
+AS
+UPDATE Toys SET UnitPrice = UnitPrice + 10 
+
+GO
+EXEC PriceIncrease 1
+
+--4
+CREATE PROCEDURE Pro_QtyOnHand 
+	@Qtyon int
+AS
+UPDATE Toys SET QtyOnHand = QtyOnHand - 5
+GO
+
+EXEC Pro_QtyOnHand 1
+
+--Phần 3
+--1
+EXEC Heavytoys '500'
+GO
+
+SELECT definition FROM sys.sql_modules
+WHERE
+object_id=OBJECT_ID('Pro_QtyOnHand')
+
+GO
+sp_depends 'Pro_QtyOnHand'
+
+GO
+SELECT 
+OBJECT_DEFINITION(OBJECT_ID('Pro_QtyOnHand')) AS DEFINITION
+
+
+--2
+EXEC sp_depends @objname = N'Toys'
+
+--3
+ALTER PROCEDURE PriceIncrease
+	@Uniprice money
+AS
+BEGIN
+	UPDATE Toys SET UnitPrice = UnitPrice + 10 
+	SELECT UnitPrice FROM Toys
+END
+GO
+
+AlTER PROCEDURE Pro_QtyOnHand 
+	@Qtyon int
+AS
+	UPDATE Toys SET QtyOnHand = QtyOnHand - 5
+	SELECT QtyOnHand FROM Toys
+GO
+
+--4
+CREATE PROC SpecificPriceIncrease
+	@spec int
+AS
+BEGIN 
+	UPDATE Toys SET UnitPrice = QtyOnHand + UnitPrice
+END
+EXEC SpecificPriceIncrease 1
+
+--5
+ALTER PROC SpecificPriceIncrease
+	@spec int,
+	@count int OUTPUT
+AS
+	SELECT @count=COUNT(*) FROM Toys
+	UPDATE Toys SET UnitPrice = QtyOnHand + UnitPrice
+	
+GO
+DECLARE @num int
+EXEC SpecificPriceIncrease 1, @num OUTPUT
+PRINT @num
+
+--6
+ALTER PROC SpecificPriceIncrease
+	@spec int
+AS
+BEGIN 
+	EXEC HeavyToys @name = @spec
+END
+GO
+EXEC SpecificPriceIncrease 500
+
+--7
+DROP PROC SpecificPriceIncrease 
+DROP PROC Heavytoys
+DROP PROC [dbo].[PriceIncrease]
+DROP PROC [dbo].[Pro_QtyOnHand]
---
+++ Task 11/triger.sql
@@ -0,0 +1,120 @@
+﻿CREATE DATABASE Triger
+
+USE Triger
+
+CREATE TABLE Employee(
+EmployeeID int PRIMARY KEY,
+Name varchar(100),
+Tel char(11),
+Email varchar(30)
+)
+GO
+INSERT INTO Employee VALUES('100','Hoàng','023654','hoang@gmail.com')
+INSERT INTO Employee VALUES('101','Minh','023654','hoang@gmail.com')
+INSERT INTO Employee VALUES('102','Ngọc','023654','hoang@gmail.com')
+INSERT INTO Employee VALUES('103','Anh','023654','hoang@gmail.com')
+
+
+CREATE TABLE Project(
+ProjectID int PRIMARY KEY,
+ProjectName varchar(30),
+StartDate datetime,
+EndDate datetime,
+Period int,
+Cost money
+)
+INSERT INTO Project VALUES('333','kỹ thuật','01/20/2010','03/05/2021',15,50000)
+INSERT INTO Project VALUES('444','maketing','01/20/2014','03/05/2021',15,60000)
+INSERT INTO Project VALUES('555','Nhà ở','01/20/2008','03/05/2021',15,70000)
+INSERT INTO Project VALUES('666','máy tính','01/20/2018','03/05/2021',15,10000)
+
+
+
+CREATE TABLE Groups(
+GroupID int PRIMARY KEY,
+GroupName varchar(30),
+LeaderID int,
+ProjectID int,
+CONSTRAINT FK_PRO FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID),
+CONSTRAINT FK_lead FOREIGN KEY (LeaderID) REFERENCES Employee(EmployeeID),
+)
+INSERT INTO Groups VALUES (401,'nhóm 1','100','333')
+INSERT INTO Groups VALUES (402,'nhóm 2','101','444')
+INSERT INTO Groups VALUES (403,'nhóm 3','102','555')
+INSERT INTO Groups VALUES (404,'nhóm 4','102','555')
+
+
+CREATE TABLE GroupDetail(
+GroupID int,
+EmployeeID int,
+Status char(20),
+PRIMARY KEY (GroupID,EmployeeID),
+CONSTRAINT FK_Gr FOREIGN KEY (GroupID) REFERENCES Groups(GroupID),
+CONSTRAINT FK_em FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
+)
+
+INSERT INTO GroupDetail VALUES (401,'100','inprogress')
+INSERT INTO GroupDetail VALUES (402,'101','done')
+INSERT INTO GroupDetail VALUES (403,'102','pending')
+INSERT INTO GroupDetail VALUES (404,'102','pending')
+
+
+--3a
+SELECT * FROM Employee
+
+--3b
+SELECT a.EmployeeID,a.Name,c.ProjectName FROM Employee AS a JOIN Groups AS b ON a.EmployeeID = b.LeaderID
+										  JOIN Project AS c ON c.ProjectID = b.ProjectID
+WHERE c.ProjectName = 'maketing'	
+
+--3c
+SELECT GroupName,COUNT(LeaderID) AS so_luong FROM Groups
+GROUP BY GroupName
+
+--3d
+SELECT b.GroupName,EmployeeID,Name,Tel,Email FROM Employee AS a JOIN Groups AS b ON a.EmployeeID = b.LeaderID
+
+--3e
+SELECT a.GroupName,c.EmployeeID,b.StartDate,b.Period,b.Cost FROM Groups AS a JOIN Project AS b ON a.ProjectID = b.ProjectID
+																   JOIN GroupDetail AS c ON c.EmployeeID = a.LeaderID
+WHERE b.StartDate < '12/10/2010'
+GROUP BY a.GroupName,c.EmployeeID,b.StartDate,b.Period,b.Cost
+
+--3g
+SELECT d.EmployeeID,a.Name,a.Tel,a.Email,b.GroupName,c.ProjectName,d.Status FROM Employee AS a JOIN Groups AS b ON a.EmployeeID = b.LeaderID
+																				 JOIN Project AS c ON c.ProjectID = b.ProjectID
+																				 JOIN GroupDetail AS d ON d.EmployeeID = b.LeaderID
+GROUP BY d.EmployeeID,a.Name,a.Tel,a.Email,b.GroupName,c.ProjectName,d.Status
+
+--4a
+ALTER TABLE Project ADD CHECK (EndDate > StartDate)
+--4b	
+ALTER TABLE Employee ALTER COLUMN Name varchar(100) NOT NULL;
+--4c
+ALTER TABLE GroupDetail ADD CHECK (Status = 'inprogress' or Status = 'pending' or Status = 'done') 
+--4d
+ALTER TABLE Project ADD CHECK (Cost > 9000)
+--4e
+SELECT LeaderID,EmployeeID FROM Employee AS a JOIN Groups AS b ON a.EmployeeID = b.LeaderID
+GROUP BY LeaderID,EmployeeID
+--4f
+SELECT Tel FROM Employee
+WHERE Tel like '0%'
+--5a
+CREATE PROC sp_chan  
+AS
+BEGIN
+	UPDATE Project SET Cost = Cost * 0.1 WHERE (Cost < 20000)
+END
+--6a
+CREATE INDEX IX_Group ON GroupDetail(GroupID,EmployeeID)
+--6b
+CREATE INDEX IX_Projec ON Project(ProjectName,StartDate,EndDate)
+--7a
+CREATE VIEW view_sa AS
+SELECT EmployeeID,Name,Tel,Email,GroupName FROM Employee AS a JOIN Groups AS b ON a.EmployeeID = b.LeaderID
+--7b
+CREATE VIEW view_dd AS
+SELECT Name,ProjectName,GroupName FROM Employee AS a JOIN Groups AS b ON a.EmployeeID = b.LeaderID
+													JOIN Project AS c ON c.ProjectID = b.ProjectID
+
